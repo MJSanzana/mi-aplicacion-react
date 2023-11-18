6b9b8c097d0a3b0073940c2bb114dbd12a4b9a16
MJSanzana <m.sanzanaalegria@uandresbello.edu>
2023-11-18 20:27:29 -0300

    frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,90 +1,30 @@
-//frontend/src/App.js
+// frontend/src/App.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Carrito from './pages/Carrito';
-import Navbar from './components/user/Navbar';
-import Soporte from './pages/Soporte';
-import Nosotros from './pages/Nosotros';
-import Registro from './pages/Registro';
-import Seguimiento from './pages/Seguimiento';
-import MiCuenta from './pages/MiCuenta';
-import Catalogo from './pages/Catalogo';
-import Producto from './pages/Productos';
-import Establecimiento from './pages/Establecimientos';
-import MisPedidos from './pages/MisPedidos';
-import CambiosyDevoluciones from './pages/CambiosyDevoluciones';
-import FAQ from './pages/FAQ';
-import UserEnsureAuth from './components/user/ProtectedRoute';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-//administrador
-import AdminNavbar from './components/admin/AdminNavbar';
-import AdminLogin from './components/admin/AdminLogin';
-import CargaDeEstablecimiento from './components/admin/CargadeEstablecimiento';
-import AdminHome from './components/admin/AdminHome';
-import AdminApproveProducts from './components/admin/AdminApproveProducts';
-import AdminEnsureAuth from './components/admin/EnsureAuth'; // 
-import { AuthProvider } from './contexts/AuthContext';
-
-//cliente
-import ClienteNavbar from './components/cliente/ClienteNavbar';
-import ClientsLogin from './components/cliente/ClientsLogin';
-import AnalisisVentas from './components/cliente/AnalisisVentas';
-import ClienteHome from './components/cliente/ClienteHome';
-import EditarStock from './components/cliente/EditarStock';
-import CargaProducto from './components/cliente/CargaProducto';
-import ClientsEnsureAuth from './components/cliente/ClientsEnsureAuth';
+// Importar tus páginas o componentes aquí
+import Home from './components/Home';
+import UserPage from './components/UserPage';
+import AdminPage from './components/AdminPage';
+import ProviderPage from './components/ProviderPage';
+import SupportPage from './components/SupportPage';
+import Login from './components/Login';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <NavigationComponent />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Carrito" element={<Carrito />} />
-          <Route path="/Soporte" element={<Soporte />} />
-          <Route path="/Nosotros" element={<Nosotros />} />
-          <Route path="/Registro" element={<Registro />} />
-          <Route path="/Seguimiento" element={<Seguimiento />} />
-          <Route path="/Productos" element={<Producto />} />
-          <Route path="/Catalogo" element={<Catalogo />} />
-          <Route path="/PreguntasFrecuentes" element={<FAQ />} />
-          <Route path="/Establecimientos" element={<Establecimiento />} />
-          <Route path="/Mispedidos" element={<MisPedidos />} />
-          <Route path="/Cambios&devoluciones" element={<CambiosyDevoluciones />} />
-          <Route path="/MiCuenta" element={<UserEnsureAuth><MiCuenta /></UserEnsureAuth>} />
-          <Route path="/Cliente" element={<ClientsEnsureAuth><ClienteHome /></ClientsEnsureAuth>} />
-          <Route path="/Cliente/Analisisventas" element={<ClientsEnsureAuth><AnalisisVentas /></ClientsEnsureAuth>} />
-          <Route path="/Cliente/login" element={<ClientsLogin />} />
-          <Route path="/Cliente/EditarStock" element={<ClientsEnsureAuth><EditarStock /></ClientsEnsureAuth>} />
-          <Route path="/Cliente/Formularioproducto" element={<ClientsEnsureAuth><CargaProducto /></ClientsEnsureAuth>} />
-          <Route path="/admin" element={<AdminEnsureAuth><AdminHome /></AdminEnsureAuth>} />
-          <Route path="/admin/Cargadeestablecimiento" element={<AdminEnsureAuth><CargaDeEstablecimiento /></AdminEnsureAuth>} />
-          <Route path="/admin/AprobarProductos" element={<AdminEnsureAuth><AdminApproveProducts /></AdminEnsureAuth>} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/pagina-usuario" element={<UserPage />} />
+        <Route path="/pagina-administrador" element={<AdminPage />} />
+        <Route path="/pagina-proveedor" element={<ProviderPage />} />
+        <Route path="/pagina-soporte" element={<SupportPage />} />
+        {/* Puedes agregar más rutas según sea necesario */}
+      </Routes>
+    </Router>
   );
 }
-function NavigationComponent() {
-  const location = useLocation();
-
-  let CurrentNavbar;
-  if (location.pathname.startsWith('/admin')) {
-    CurrentNavbar = AdminNavbar;
-  } else if (location.pathname.startsWith('/Cliente')) {
-    CurrentNavbar = ClienteNavbar;
-  } else {
-    CurrentNavbar = Navbar;
-  }
-
-  return <CurrentNavbar />;
-}
-
 export default App;
---
+++ Frontend/src/components/Home.js
@@ -0,0 +1,99 @@
+//import Producto from '../components/Producto';
+import React from 'react';
+import Footer from '../pages/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Home extends React.Component {
+    state = {
+        cart: [],
+    };
+
+    addToCart = (name, price, quantity) => {
+        const newCart = [...this.state.cart];
+        const existingItem = newCart.find(item => item.name === name);
+
+        if (existingItem) {
+            existingItem.quantity += quantity;
+        } else {
+            newCart.push({
+                name,
+                price,
+                quantity,
+            });
+        }
+
+        this.setState({ cart: newCart });
+    };
+    render() {
+        return (
+            <div className="bg-light py-5">
+                <main className="container">
+                    <section className="mb-5 text-center">
+                        <h4 className="mt-3 display-4">Nuestra Colección de Uniformes</h4>
+                        <p className="lead">Descubre una amplia variedad de uniformes escolares de calidad, diseñados especialmente para estudiantes de las 32 Comunas de la provincia de Santiago.</p>
+                    </section>
+
+                    <section className="row mb-5">
+                        <div className="col-md-4">
+                            <div className="card h-100 shadow-sm">
+                                <div className="card-body text-center">
+                                    <i className="fas fa-school fa-3x mb-3"></i>
+                                    <h3 className="card-title">Conocenos</h3>
+                                    <p className="card-text">Conoce más sobre nuestra historia, misión y visión. Somos más que un simple establecimiento, somos una comunidad.</p>
+                                    <a href="/Nosotros"><img width="80" height="80" src="https://img.icons8.com/fluency/48/info.png" alt="Nosotros" /></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card h-100 shadow-sm">
+                                <div className="card-body text-center">
+                                    <i className="fas fa-headset fa-3x mb-3"></i>
+                                    <h3 className="card-title">Soporte</h3>
+                                    <p className="card-text">Estamos aquí para ayudarte. Si tienes alguna duda o problema, ¡no dudes en contactarnos!</p>
+                                    <a href="/Soporte"><img width="80" height="80" src="https://img.icons8.com/dusk/64/online-support--v1.png" alt="Soporte" /></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card h-100 shadow-sm">
+                                <div className="card-body text-center">
+                                    <i className="fas fa-truck fa-3x mb-3"></i>
+                                    <h3 className="card-title">Seguimiento de pedido</h3>
+                                    <p className="card-text">¿Ansioso por tu nuevo uniforme? ¡Sigue tu pedido en tiempo real!</p>
+                                    <a href="/Seguimiento"><img width="80" height="80" src="https://img.icons8.com/cotton/64/track-order.png" alt="Seguimiento" /></a>
+                                </div>
+                            </div>
+                        </div>
+                        <p></p>
+                        <div className="col-md-4">
+                            <div className="card h-100 shadow-sm">
+                                <div className="card-body text-center">
+                                    <i className="fas fa-truck fa-3x mb-3"></i>
+                                    <h3 className="card-title">Colegios</h3>
+                                    <p className="card-text">Busqueda por Establecimientos Educacionales</p>
+                                    <a href="/Establecimientos"><img width="80" height="80" src="https://img.icons8.com/color/48/classroom.png" alt="Establecimientos" /></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card h-100 shadow-sm">
+                                <div className="card-body text-center">
+                                    <i className="fas fa-truck fa-3x mb-3"></i>
+                                    <h3 className="card-title">Preguntas Frecuentes</h3>
+                                    <p className="card-text">Busqueda de Establecimiento Educacional por Comuna</p>
+                                    <a href="/PreguntasFrecuentes"><img width="80" height="80" src="https://img.icons8.com/color/48/faq.png" alt="PreguntasFrecuentas" /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+}
+export default Home;
