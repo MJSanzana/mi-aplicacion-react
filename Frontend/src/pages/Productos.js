7b7425d031f3b5a09b457c75ba7c4c6545fe4bd9
MJSanzana <m.sanzanaalegria@uandresbello.edu>
2023-11-24 08:02:14 -0300

    cambios en Frontend Productos carrito

--- Frontend/src/pages/Productos.js
+++ Frontend/src/pages/Productos.js
@@ -17,6 +17,27 @@ function Productos({ changeView }) {
   const [cantidadAComprar, setCantidadAComprar] = useState(1);
   const [stockDisponible, setStockDisponible] = useState(0);
 
+  // Función para manejar agregar productos al carrito
+  const onAddToCart = (productToAdd) => {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const existingProductIndex = cartItems.findIndex(item => item.Id === productToAdd.Id && item.talla === productToAdd.talla);
+
+    if (existingProductIndex > -1) {
+      // Producto ya existe en el carrito, actualizar la cantidad
+      const updatedItems = cartItems.map((item, index) => {
+        if (index === existingProductIndex) {
+          return { ...item, cantidad: item.cantidad + productToAdd.cantidad };
+        }
+        return item;
+      });
+
+      localStorage.setItem('cartItems', JSON.stringify(updatedItems));
+    } else {
+      // Producto no existe en el carrito, agregar nuevo
+      localStorage.setItem('cartItems', JSON.stringify([...cartItems, productToAdd]));
+    }
+  };
+
   useEffect(() => {
     fetchProductos();
   }, []);
@@ -110,120 +131,126 @@ function Productos({ changeView }) {
         setStockDisponible={setStockDisponible}
         cantidadAComprar={cantidadAComprar}
         setCantidadAComprar={setCantidadAComprar}
+        onAddToCart={onAddToCart}
       />
     </>
   );
 }
-const [cantidad, setCantidad] = useState(1);
-const navigateToCart = () => {
-  onHide();
-  changeView('ShoppingCart'); // Cambia la vista usando changeView
-};
-
-
-useEffect(() => {
-  const carrito = JSON.parse(localStorage.getItem('cartItems')) || [];
-  const productoEnCarrito = carrito.find(item => item.Id === producto.Id);
-  setCantidad(productoEnCarrito ? productoEnCarrito.cantidad : 1);
-}, [show, producto.Id]);
-
-
-
-const actualizarCarrito = (delta) => {
-  const carrito = JSON.parse(localStorage.getItem('cartItems')) || [];
-  const index = carrito.findIndex((item) => item.Id === producto.Id);
-  let nuevaCantidad = cantidad + delta;
-
-  if (index !== -1) {
-    if (nuevaCantidad > 0) {
-      carrito[index].cantidad = nuevaCantidad;
-    } else {
-      carrito.splice(index, 1);
-    }
-  } else if (nuevaCantidad > 0) {
-    carrito.push({ ...producto, cantidad: nuevaCantidad });
-  }
-
-  localStorage.setItem('cartItems', JSON.stringify(carrito));
-  setCantidad(nuevaCantidad > 0 ? nuevaCantidad : 1);
-};
-
-const incrementarCantidadAComprar = () => {
-  if (cantidadAComprar < stockDisponible) {
-    setCantidadAComprar(cantidadAComprar + 1);
-  }
-};
-const decrementarCantidadAComprar = () => {
-  if (cantidadAComprar > 1) {
-    setCantidadAComprar(cantidadAComprar - 1);
-  }
-};
-
-const agregarAlCarrito = () => {
-  if (producto && tallaSeleccionada) {
-    onAddToCart({ ...producto, cantidad, talla: tallaSeleccionada });
+const ProductoModal = ({
+  show, onHide, producto, changeView, variantes, tallaSeleccionada, setTallaSeleccionada,
+  stockDisponible, setStockDisponible, cantidadAComprar, setCantidadAComprar, onAddToCart
+}) => {
+  const [cantidad, setCantidad] = useState(1);
+  const navigateToCart = () => {
     onHide();
-  } else {
-    alert('Por favor, selecciona una talla.');
-  }
-};
-const handleTallaChange = (talla) => {
-  setTallaSeleccionada(talla);
-  const variante = variantes.find(v => v.Talla === talla);
-  setStockDisponible(variante ? variante.Cantidad : 0);
-  setCantidadAComprar(1); // Reiniciar la cantidad a comprar al cambiar de talla
-};
+    changeView('ShoppingCart'); // Cambia la vista usando changeView
+  };
 
 
-return (
-  <Modal show={show} onHide={onHide} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
-    <Modal.Header closeButton>
-      <Modal.Title id="contained-modal-title-vcenter">
-        {producto?.Nombre || 'Cargando...'}
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      {producto && (
-        <>
-          <Image src={getImageUrl(producto.Imagen)} alt={`Imagen de ${producto.Nombre}`} className="modal-image" fluid />
-          <p>Precio: ${producto.Precio?.toLocaleString() || 'No disponible'}</p>
-          <div>
-            <label htmlFor="tallaSelect">Talla:</label>
-            <select
-              className="form-control"
-              id="tallaSelect"
-              value={tallaSeleccionada}
-              onChange={(e) => handleTallaChange(e.target.value)}
-              required
-            >
-              <option value="" disabled>Seleccione una talla</option>
-              {variantes.map((variante, index) => (
-                <option key={index} value={variante.Talla}>
-                  {variante.Talla}
-                </option>
-              ))}
-            </select>
-            <p>Cantidad disponible: {stockDisponible}</p>
-          </div>
-          <div className="d-flex align-items-center justify-content-center">
-            <Button onClick={decrementarCantidadAComprar} disabled={cantidadAComprar <= 1}>-</Button>
-            <span className="mx-2">{cantidadAComprar}</span>
-            <Button onClick={incrementarCantidadAComprar} disabled={cantidadAComprar >= stockDisponible}>+</Button>
-          </div>
-        </>
-      )}
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="primary" onClick={agregarAlCarrito}>
-        Agregar al Carrito
-      </Button>
-      <Button variant="secondary" onClick={navigateToCart}>
-        Ir al Carro
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+  useEffect(() => {
+    const carrito = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const productoEnCarrito = carrito.find(item => item.Id === producto.Id);
+    setCantidad(productoEnCarrito ? productoEnCarrito.cantidad : 1);
+  }, [show, producto.Id]);
 
+
+
+  const actualizarCarrito = (delta) => {
+    const carrito = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const index = carrito.findIndex((item) => item.Id === producto.Id);
+    let nuevaCantidad = cantidad + delta;
+
+    if (index !== -1) {
+      if (nuevaCantidad > 0) {
+        carrito[index].cantidad = nuevaCantidad;
+      } else {
+        carrito.splice(index, 1);
+      }
+    } else if (nuevaCantidad > 0) {
+      carrito.push({ ...producto, cantidad: nuevaCantidad });
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(carrito));
+    setCantidad(nuevaCantidad > 0 ? nuevaCantidad : 1);
+  };
+
+  const incrementarCantidadAComprar = () => {
+    if (cantidadAComprar < stockDisponible) {
+      setCantidadAComprar(cantidadAComprar + 1);
+    }
+  };
+  const decrementarCantidadAComprar = () => {
+    if (cantidadAComprar > 1) {
+      setCantidadAComprar(cantidadAComprar - 1);
+    }
+  };
+
+  const agregarAlCarrito = () => {
+    if (producto && tallaSeleccionada && cantidadAComprar > 0) {
+      // Aquí usas la función onAddToCart pasada como prop
+      onAddToCart({ ...producto, cantidad: cantidadAComprar, talla: tallaSeleccionada });
+      onHide(); 
+    } else {
+      alert('Por favor, selecciona una talla y asegúrate de que la cantidad sea mayor que 0.');
+    }
+  };
+
+  const handleTallaChange = (talla) => {
+    setTallaSeleccionada(talla);
+    const variante = variantes.find(v => v.Talla === talla);
+    setStockDisponible(variante ? variante.Cantidad : 0);
+    setCantidadAComprar(1); // Reiniciar la cantidad a comprar al cambiar de talla
+  };
+
+
+  return (
+    <Modal show={show} onHide={onHide} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {producto?.Nombre || 'Cargando...'}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {producto && (
+          <>
+            <Image src={getImageUrl(producto.Imagen)} alt={`Imagen de ${producto.Nombre}`} className="modal-image" fluid />
+            <p>Precio: ${producto.Precio?.toLocaleString() || 'No disponible'}</p>
+            <div>
+              <label htmlFor="tallaSelect">Talla:</label>
+              <select
+                className="form-control"
+                id="tallaSelect"
+                value={tallaSeleccionada}
+                onChange={(e) => handleTallaChange(e.target.value)}
+                required
+              >
+                <option value="" disabled>Seleccione una talla</option>
+                {variantes.map((variante, index) => (
+                  <option key={index} value={variante.Talla}>
+                    {variante.Talla}
+                  </option>
+                ))}
+              </select>
+              <p>Cantidad disponible: {stockDisponible}</p>
+            </div>
+            <div className="d-flex align-items-center justify-content-center">
+              <Button onClick={decrementarCantidadAComprar} disabled={cantidadAComprar <= 1}>-</Button>
+              <span className="mx-2">{cantidadAComprar}</span>
+              <Button onClick={incrementarCantidadAComprar} disabled={cantidadAComprar >= stockDisponible}>+</Button>
+            </div>
+          </>
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={agregarAlCarrito}>
+          Agregar al Carrito
+        </Button>
+        <Button variant="secondary" onClick={navigateToCart}>
+          Ir al Carro
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default Productos;
